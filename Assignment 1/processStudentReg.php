219005486adc374abc8ae71044c9f663de6f7a58
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2023-09-16 16:09:56 -0300

    w

--- Assignment 1/processStudentReg.php
+++ Assignment 1/processStudentReg.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Holland College Student Registration</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
+<main style=" display: flex; justify-content: center; align-items: center; height: 100%;">
+    <div style="width: 40%">
+        <h1>Holland College</h1>
 <?php
 
 if (isset($_POST["fName"])
@@ -15,16 +16,24 @@ if (isset($_POST["fName"])
     && isset($_POST['aName'])
     && isset($_POST['aNumber'])
     && isset($_POST['aPCode'])
+    && isset($_POST['aCity'])
+    && isset($_POST['aProvince'])
     && isset($_POST['pNumber'])
+    && isset($_POST['semesters'])
+    && isset($_POST['cPerSemester'])
+    && isset($_POST['HCDeduction'])
+    && isset($_POST['Scholarship'])
 ) {
     echo '<h2 style="color: #69a45a">Submitted Successfully!!!</h2>';
 
 
 } else {
 
-    echo "<p>Please Go back to the Form and submit it with information</p> <a href='studentReg.html'>Back to Form</a>";
+    echo "<p>Please Go back to the Form and fill out everything!</p> <a href='studentReg.html'>Back to Form</a>";
     //include_once 'studentReg.html';
 }
 ?>
+<div>
+</main>
 </body>
 </html>
