a6ab90e1887af35d1ca86afe61c861fc3ba8786e
Joshua Mckenna <joshuamckenna7@gmail.com>
2023-10-21 20:22:08 -0300

    w

--- Cis-2288 A3 Mckenna Joshua/orderForm.php
+++ Cis-2288 A3 Mckenna Joshua/orderForm.php
@@ -3,44 +3,76 @@
 require_once 'util/CisUtil.php';
 require_once 'util/FormBuilder.php';
 
-CisUtil::startPage("Acme Accessories Inc.", '<link href="css/Custom.css" rel="stylesheet">');
+CisUtil::startPage("Acme Accessories Inc.", '
+<link href="css/Custom.css" rel="stylesheet">
+<style>
+thead,
+tfoot {
+  background-color: #3f87a6;
+  color: #fff;
+}
+
+tbody {
+  background-color: #e4f0f5;
+}
+
+caption {
+  padding: 10px;
+  caption-side: bottom;
+}
+
+table {
+  border-collapse: collapse;
+  border: 2px solid rgb(200, 200, 200);
+  letter-spacing: 1px;
+  font-family: sans-serif;
+  font-size: 0.8rem;
+}
+
+td,
+th {
+  border: 1px solid rgb(190, 190, 190);
+  padding: 5px 10px;
+}
+
+td {
+  text-align: center;
+}
+
+</style>
+');
 
 CisUtil::navbar();        /*Image Credit
         https://www.apple.com/ca/newsroom/2023/09/apple-unveils-iphone-15-pro-and-iphone-15-pro-max/
         */
+
 ?>
-<div
-
-
-    class="bg-image container container-center"
-    style="
-
-        background-image: url('photos/Header-Image.jpg');
-        height: 200px;
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-    "
->
-
-    <div class="text-white">
-        <h1 class="mb-3 border-bottom border-2 border-white display-4">Acme Accessories</h1>
-    </div>
-
 </div>
-<div class="container fill-page bg-white">
+<?php CisUtil::header("Acme Accessories" ) ?>
+<main class="container fill-page bg-white">
+    <form action="" method="post">
+    <table>
+        <caption>
+            Council budget (in £) 2018
+        </caption>
+        <thead>
+        <tr>
+            <th scope="col">Items</th>
+            <th scope="col">Expenditure</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>3,000</td>
+        </tr>
+        <tr>
+            <th scope="row">Stationery</th>
+            <td>18,000</td>
+        </tr>
+        </tbody>
+    </table>
 
-    <?php
-    $form = new FormBuilder("processOrder.php")
-
-
-    ?>
-
-</div>
+</main>
 
 
 <?php
